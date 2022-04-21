58adda719bf5ebc48f034e47ded59a4edb58f081
AlexBugaev1990 <alex.bugaev90@gmail.com>
2022-04-21 20:36:22 +0300

    Добавьте файлы проекта.

---
+++ Task-4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = (byte) int.Parse(Console.ReadLine());
+            Console.WriteLine("Your name is {0} and age is {1}", name, age);
+            Console.WriteLine("Enter your birthdate:");
+            string birthdate = Console.ReadLine();
+            Console.WriteLine("Your birthdate is {0}", birthdate);
+        }
+    }
+}
